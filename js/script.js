781e536b047b19619dc2d9e140d81a240960269f
Sekvosha <little.sekvosha@gmail.com>
2025-07-07 16:53:59 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 const config = {
-      repoOwner: 'Sekvosha',
-      repoName: 'crimea-tree',
-      branch: 'main',
-      files: [
-        'data/hello.md',
-        'data/second.md'
-    ]
+    owner: 'Sekvosha',
+    repo: 'crimea-tree',
+    branch: 'main',
+    folder: 'data'
 };
 
 const file_container = document.getElementById('file_div');
 
-async function GetFileContent(file_path) {
-    const url = `https://raw.githubusercontent.com/${config.repoOwner}/${config.repoName}/${config.branch}/${file_path}`;
+async function getFileTree(){
+    try {
+        const response = await fetch(`https://api.github.com/repos/${config.owner}/${config.repo}/contents/${config.folder}/`);
+
+        if (!response.ok) {
+            throw new Error(`Ошибка HTTP: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+         // Убедимся, что data - массив
+        if (!Array.isArray(data)) {
+            throw new Error('GitHub API вернул неожиданные данные (ожидался массив)');
+        }
+        console.log('get file tree: SUCCES')
+        return data.filter(item => item.type === 'file').map(item => item.path);
+    }catch(error){
+        console.error('Ошибка при получении дерева файлов:', error);
+        throw error;
+    }
+}
+
+async function GetFileContent(file_path){
+    const url = `https://raw.githubusercontent.com/${config.owner}/${config.repo}/${config.branch}/${file_path}`;
     try{
         const response = await fetch(url);
 
@@ -25,13 +44,31 @@ async function GetFileContent(file_path) {
     }
 }
 
-async function DisplayFiles() {
-    for(const file_path of config.files){
+
+
+async function DisplayFiles(){
+    const files = await getFileTree();
+    console.log('Получен список файлов: ', files)
+    for(const file_path of files){
         const file_block = document.createElement(`div`);
         file_block.className = 'file_div';
+
+        header = file_path.replace(config.folder+'/', '')
+        header = header.replace('.md', '')
+        header = header.charAt(0).toUpperCase() + header.slice(1)
+        header = '🌲 ' + header;
+
         file_block.innerHTML = `
-        <h3>${file_path}</h3>
-        <div class="loading">Загрузка...</div>
+            <div class="t004">
+
+                <div class="t-container ">
+                <div class="t-col t-col_8 t-prefix_2">
+                    <h3>${header}</h3>
+                    <div field="text" class="loading ">Загрузка...</div>
+                </div>
+                </div>
+            </div>
+            <br>
         `;
         file_container.appendChild(file_block);
 
@@ -39,6 +76,10 @@ async function DisplayFiles() {
 
         const content_element = document.createElement('pre');
         content_element.textContent = content;
+        content_element.classList.add('t-text');
+        content_element.classList.add('t-text-md');
+        content_element.classList.add('padd');
+        
         file_block.querySelector('.loading').replaceWith(content_element);
     }
 }
